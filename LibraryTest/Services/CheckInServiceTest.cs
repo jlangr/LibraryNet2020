9107701db67d02ddaa39d6a72a2d106f2baa04db
Jeff L <jeff@langrsoft.com>
2020-11-03 11:32:06 -0700

    check in service test validations

--- LibraryTest/Services/CheckInServiceTest.cs
+++ LibraryTest/Services/CheckInServiceTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using LibraryNet2020.Controllers;
 using LibraryNet2020.Models;
 using LibraryNet2020.Services;
@@ -30,6 +31,44 @@ namespace LibraryTest.Services
             Assert.True(checkInService.Checkin(checkin));
             
             holdingsServiceMock.Verify(s => s.CheckIn(holding, 42));
+        }
+        
+        [Fact]
+        public void ValidatesBarcode()
+        {
+            var checkInService = new CheckInService(context, holdingsServiceMock.Object);
+            var checkin = new CheckInViewModel { Barcode = "QA1231" };
+
+            Assert.False(checkInService.Checkin(checkin));
+            
+            Assert.Equal("Invalid holding barcode format: QA1231", checkInService.ErrorMessages.First());
+        }
+
+        [Fact]
+        public void ValidatesHoldingRetrieval()
+        {
+            SavePatronWithId(context, 5);
+            var checkInService = new CheckInService(context, holdingsServiceMock.Object);
+            var checkin = new CheckInViewModel { Barcode = "QA123:1" };
+
+            Assert.False(checkInService.Checkin(checkin));
+            
+            Assert.Equal("Holding with barcode QA123:1 not found", 
+                checkInService.ErrorMessages.First());
+        }
+
+        [Fact]
+        public void ValidatesHoldingAvailability()
+        {
+            SaveCheckedOutHoldingWithClassification(context, "QA123");
+            SavePatronWithId(context, 5);
+            var checkInService = new CheckInService(context, holdingsServiceMock.Object);
+            var checkin = new CheckInViewModel { Barcode = "QA123:1" };
+
+            Assert.False(checkInService.Checkin(checkin));
+            
+            Assert.Equal("Holding with barcode QA123:1 is already checked out.", 
+                checkInService.ErrorMessages.First());
         }
     }
 }
