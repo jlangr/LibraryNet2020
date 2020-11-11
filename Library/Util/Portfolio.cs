b91f6555793fd02e3a0f5e98c2c68ccb7ccc1f39
Charlie Holmes <charles.holmes@bayer.com>
2020-11-11 15:04:43 -0600

    mob next [ci-skip]

---
+++ Library/Util/Portfolio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryNet2020.Util
+{
+    public class Portfolio
+    {
+        public bool IsEmpty { get; set; } = true;
+
+        public int Count => 0;
+
+        public void Purchase()
+        {
+            IsEmpty = false;
+        }
+    }
+}
---
+++ LibraryTest/Util/PortfolioTest.cs
@@ -0,0 +1,37 @@
+﻿using LibraryNet2020.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LibraryTest.Util
+{
+    public class PortfolioTest
+    {
+        private Portfolio portfolio;
+        public PortfolioTest()
+        {
+            portfolio = new Portfolio();
+        }
+
+        [Fact]
+        public void IsEmptyWhenNothingPurchased()
+        {
+            Assert.True(portfolio.IsEmpty);
+        }
+
+        [Fact]
+        public void IsNoLongerEmptyAfterPurchase()
+        {
+            portfolio.Purchase();
+
+            Assert.False(portfolio.IsEmpty);
+        }
+
+        [Fact]
+        public void SymbolCountIsZeroWhenNothingPurchased()
+        {
+            Assert.Equal(0, portfolio.Count);
+        }
+    }
+}
