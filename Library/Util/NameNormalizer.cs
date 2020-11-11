cdfcc15ec914924084fee81e5fd28532ea817c01
Randy Belen <randy.belen@monsanto.com>
2020-11-11 11:17:44 -0600

    mob next [ci-skip]

--- Library/Util/NameNormalizer.cs
+++ Library/Util/NameNormalizer.cs
@@ -1,25 +1,38 @@
-﻿namespace LibraryNet2020.Util
+﻿using System.Text;
+
+namespace LibraryNet2020.Util
 {
     public class NameNormalizer
 	{
         public string Normalize(string unnormalizedName)
         {
             var parts = Parts(unnormalizedName);
-            if (IsMononym(parts))
+            if (parts.Length == 1)
                 return unnormalizedName;
-            return Last(parts) + ", " + First(parts);
+            else if (parts.Length == 2)
+            {
+                return Last(parts) + ", " + First(parts);
+            }
+
+            return Last(parts) + ", " + First(parts) + " " + GetMiddleNames(parts);            
+        }
+
+        private static string GetMiddleNames(string[] parts)
+        {
+            StringBuilder middleNameInitials = new StringBuilder();
+            
+            for (int i = 1; i < parts.Length-1; i++)
+            {
+                middleNameInitials.Append($"{parts[i][0]}. ");
+            }
+
+            return middleNameInitials.ToString().Trim();
         }
 
         private string[] Parts(string name)
         {
             return name.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        private static bool IsMononym(string[] parts)
-        {
-            return parts.Length != 2;
-        }
-
+        }           
         private static string First(string[] parts)
         {
             return parts[0];
@@ -27,7 +40,7 @@
 
         private static string Last(string[] parts)
         {
-            return parts[1];
+            return parts[parts.Length - 1];
         }
     }
 }
