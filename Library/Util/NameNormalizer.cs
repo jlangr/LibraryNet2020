2d0ee4fae9b66de2920072e035e379233d6af146
Charlie Holmes <charles.holmes@bayer.com>
2020-11-11 11:44:29 -0600

    mob next [ci-skip]

--- Library/Util/NameNormalizer.cs
+++ Library/Util/NameNormalizer.cs
@@ -3,36 +3,58 @@
 namespace LibraryNet2020.Util
 {
     public class NameNormalizer
-	{
+    {
         public string Normalize(string unnormalizedName)
         {
-            var parts = Parts(unnormalizedName);
-            if (parts.Length == 1)
-                return unnormalizedName;
-            else if (parts.Length == 2)
+            var parts = SplitByComma(unnormalizedName);
+            for (int i = 1; i < parts.Length; i++)
             {
-                return Last(parts) + ", " + First(parts);
-            }
+                if (parts[i] == "Jr.")
+                {
 
-            return Last(parts) + ", " + First(parts) + " " + GetMiddleNames(parts);            
+                }
+                else
+                {
+                    var regularName = SplitBySpace(unnormalizedName);
+
+                    if (regularName.Length == 1)
+                        return unnormalizedName;
+                    else if (regularName.Length == 2)
+                    {
+                        return Last(regularName) + ", " + First(regularName);
+                    }
+
+                    return Last(regularName) + ", " + First(regularName) + " " + GetMiddleNames(regularName);
+
+                }
+            }
         }
 
         private static string GetMiddleNames(string[] parts)
         {
             StringBuilder middleNameInitials = new StringBuilder();
-            
-            for (int i = 1; i < parts.Length-1; i++)
+
+            for (int i = 1; i < parts.Length - 1; i++)
             {
-                middleNameInitials.Append($"{parts[i][0]}. ");
+                if (parts[i].Length > 1)
+                    middleNameInitials.Append($"{parts[i][0]}. ");
+                else
+                    middleNameInitials.Append($"{parts[i][0]} ");
             }
 
             return middleNameInitials.ToString().Trim();
         }
 
-        private string[] Parts(string name)
+        private string[] SplitBySpace(string name)
         {
             return name.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
-        }           
+        }
+
+        private string[] SplitByComma(string name)
+        {
+            return name.Split(',', System.StringSplitOptions.RemoveEmptyEntries);
+        }
+
         private static string First(string[] parts)
         {
             return parts[0];
