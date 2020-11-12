9d8df674118c2b6cda20cfe9b4877493153d85be
Charlie Holmes <charles.holmes@bayer.com>
2020-11-12 15:11:08 -0600

    mob next [ci-skip]

---
+++ Library/Util/Portfolio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace LibraryNet2020.Util
+{
+    public class Portfolio
+    {
+        public bool IsEmpty => Count == 0;
+
+        public int Count => symbols.Count;
+
+        public int Value => 0;
+
+        private HashSet<string> symbols = new HashSet<string>();
+
+        public void Purchase(string symbol = "")
+        {
+            symbols.Add(symbol);
+        }
+    }
+}
---
+++ LibraryTest/Util/PortfolioTest.cs
@@ -0,0 +1,73 @@
+﻿using LibraryNet2020.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace LibraryTest.Util
+{
+    public class PortfolioTest
+    {
+        private Portfolio portfolio;
+
+        public PortfolioTest()
+        {
+            portfolio = new Portfolio();
+        }
+
+        [Fact]
+        public void IsEmptyWhenNothingPurchased()
+        {
+            Assert.True(portfolio.IsEmpty);
+        }
+
+        [Fact]
+        public void IsNoLongerEmptyAfterPurchase()
+        {
+            portfolio.Purchase();
+
+            Assert.False(portfolio.IsEmpty);
+        }
+
+        [Fact]
+        public void SymbolCountIsZeroWhenNothingPurchased()
+        {
+            Assert.Equal(0, portfolio.Count);
+        }
+
+        [Fact]
+        public void SymbolCountIsNotZeroAfterPurchase()
+        {
+            portfolio.Purchase();
+
+            Assert.NotEqual(0, portfolio.Count);
+        }
+
+        [Fact]
+        public void SymbolCountIncrementsWithPurchaseOfUniqueSymbol()
+        {
+            portfolio.Purchase("yolo");
+            var countBeforeUniquePurchase = portfolio.Count;
+
+            portfolio.Purchase("notyolo");
+
+            Assert.Equal(countBeforeUniquePurchase + 1, portfolio.Count);
+        }
+
+        [Fact]
+        public void SymbolCountDoesNotIncrementWithPurchaseOfSameSymbol()
+        {
+            portfolio.Purchase("yolo");
+            var countBeforeUniquePurchase = portfolio.Count;
+
+            portfolio.Purchase("yolo");
+
+            Assert.Equal(countBeforeUniquePurchase, portfolio.Count);
+        }
+
+        [Fact]
+        public void ValueIsZeroWhenPortfolioIsEmpty()
+        {
+            Assert.Equal(0, portfolio.Value);
+        }
+    }
+}
